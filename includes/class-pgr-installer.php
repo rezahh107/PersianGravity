ba922970db953365421a77579c7173c1d90380eb
rezahh107 <84147076+rezahh107@users.noreply.github.com>
2026-05-05 13:10:28 +0330

    Update class-pgr-installer.php

--- includes/class-pgr-installer.php
+++ includes/class-pgr-installer.php
@@ -1,10 +1,9 @@
-
 <?php
 /**
  * Installer/Activator for Persian Gravity Forms Refactor.
  *
  * @package PersianGravityFormsRefactor
- * @since   1.0.0
+ * @since   3.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -12,18 +11,24 @@ defined( 'ABSPATH' ) || exit;
 class PGR_Installer {
 
     public static function activate() : void {
+        // PHP version check
         if ( version_compare( PHP_VERSION, '7.4', '<' ) ) {
             deactivate_plugins( plugin_basename( PGR_FILE ) );
             wp_die( esc_html__( 'Persian Gravity Forms Refactor requires PHP 7.4 or newer.', 'persian-gravityforms-refactor' ) );
         }
 
+        // WordPress version check
         global $wp_version;
         if ( version_compare( $wp_version, '5.8', '<' ) ) {
             deactivate_plugins( plugin_basename( PGR_FILE ) );
             wp_die( esc_html__( 'Persian Gravity Forms Refactor requires WordPress 5.8 or newer.', 'persian-gravityforms-refactor' ) );
         }
 
-        add_option( 'pgr_version', '1.0.0' );
+        // Store plugin version
+        update_option( 'pgr_version', '3.0.0' );
+        
+        // Clean up old options from previous versions (if any)
+        delete_option( 'pgr_installed' );
     }
 
     public static function uninstall() : void {
@@ -32,6 +37,7 @@ class PGR_Installer {
         }
 
         delete_option( 'pgr_version' );
+        delete_option( 'pgr_settings' );
         delete_option( 'pgr_installed' );
     }
 }
