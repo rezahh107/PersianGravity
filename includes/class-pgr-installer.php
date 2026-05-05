3cc5434194b971679e95398be9f17ef580688a26
rezahh107 <84147076+rezahh107@users.noreply.github.com>
2026-05-05 13:16:57 +0330

    Update class-pgr-installer.php

--- includes/class-pgr-installer.php
+++ includes/class-pgr-installer.php
@@ -11,23 +11,18 @@ defined( 'ABSPATH' ) || exit;
 class PGR_Installer {
 
     public static function activate() : void {
-        // PHP version check
         if ( version_compare( PHP_VERSION, '7.4', '<' ) ) {
             deactivate_plugins( plugin_basename( PGR_FILE ) );
             wp_die( esc_html__( 'Persian Gravity Forms Refactor requires PHP 7.4 or newer.', 'persian-gravityforms-refactor' ) );
         }
 
-        // WordPress version check
         global $wp_version;
         if ( version_compare( $wp_version, '5.8', '<' ) ) {
             deactivate_plugins( plugin_basename( PGR_FILE ) );
             wp_die( esc_html__( 'Persian Gravity Forms Refactor requires WordPress 5.8 or newer.', 'persian-gravityforms-refactor' ) );
         }
 
-        // Store plugin version
         update_option( 'pgr_version', '3.0.0' );
-        
-        // Clean up old options from previous versions (if any)
         delete_option( 'pgr_installed' );
     }
 
