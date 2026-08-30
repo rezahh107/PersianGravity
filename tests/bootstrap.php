53ef2ddec825a277b4f730c770c24ee74e2c4d1e
rezahh107 <84147076+rezahh107@users.noreply.github.com>
2026-08-30 22:17:29 +0330

    Remove obsolete parallel and legacy architectures

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// راه‌اندازی Brain Monkey برای تست هوک‌های وردپرس
-if (class_exists('Brain\Monkey')) {
-    \Brain\Monkey\setUp();
-} 
